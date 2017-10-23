befa885d95085bf9a177786b668686c66f3186c6
Spruudel <n.spalthoff@web.de>
2017-10-24 01:45:38 +0200

    cleaned up code

--- particle.js
+++ particle.js
@@ -1,42 +1,37 @@
-var maxDistance = 150;
-
 function Particle(x,y) {
     this.pos = createVector(x,y);
     this.vel = p5.Vector.random2D();
     this.vel.mult(0.25);
     this.r = 3;
-    this.R = 0;
-    this.G = 0;
-    this.B = 0;
-    this.A = 150;
+    this.alpha = 150;
 
     this.move = function() {
         this.pos.add(this.vel);
     }
 
     this.show = function() {
-        fill(this.R, this.G, this.B, this.A);
+        fill(0, 0, 0, this.alpha);
         ellipse(this.pos.x, this.pos.y, this.r, this.r);
     }
 
     this.offScreen = function() {
-      if (this.pos.x + this.r < 0 || this.pos.x - this.r > width || this.pos.y + this.r < 0 || this.pos.y - this.r > height) {
-        return true;
-      } else {
-        return false;
-      }
+        if (this.pos.x + this.r < 0 || this.pos.x - this.r > width || this.pos.y + this.r < 0 || this.pos.y - this.r > height) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     this.checkDist = function() {
-      var neighbours = [];
+        var neighbours = [];
 
-      for (i = 0; i < particles.length; i++) {
-          var distance = dist(this.pos.x, this.pos.y, particles[i].pos.x, particles[i].pos.y);
-          if (distance < maxDistance && distance > 0) {
-              neighbours.push(i);
-          }
-      }
+        for (i = 0; i < particles.length; i++) {
+            var distance = dist(this.pos.x, this.pos.y, particles[i].pos.x, particles[i].pos.y);
+            if (distance < maxDistance && distance > 0) {
+                neighbours.push(i);
+            }
+        }
 
-      return neighbours;
+        return neighbours;
     }
 }
