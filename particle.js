3a25fd4bae13fe0c55ad12c44e060cd1a1ae20af
Spruudel <n.spalthoff@web.de>
2018-05-18 17:20:05 +0200

    started to change dist method

--- particle.js
+++ particle.js
@@ -5,6 +5,16 @@ function Particle(x, y) {
     this.r = 3;
     this.alpha = 150;
 
+	this.gridPos = function() {
+		let pX = floor(this.pos.x / maxDistance);
+		let pY = floor(this.pos.y / maxDistance);
+
+		if (pX < 0) pX = 0;
+		if (pY < 0) pY = 0;
+
+		return [pX, pY];
+	}
+
     this.move = function() {
         this.pos.add(this.vel);
     }
@@ -15,7 +25,9 @@ function Particle(x, y) {
     }
 
     this.offScreen = function() {
-        if (this.pos.x + this.r < 0 || this.pos.x - this.r > width || this.pos.y + this.r < 0 || this.pos.y - this.r > height) {
+		let nextPos = this.pos.add(this.vel);
+
+        if (nextPos.x <= 0 || nextPos.x >= width || nextPos.y <= 0 || nextPos.y >= height) {
             return true;
         } else {
             return false;
@@ -23,11 +35,29 @@ function Particle(x, y) {
     }
 
     this.checkDist = function() {
-        var neighbours = [];
+        let neighbours = [];
+		let [gX, gY] = this.gridPos();
+		let toCheck = [];
 
-        for (i = 0; i < particles.length; i++) {
-            var distance = dist(this.pos.x, this.pos.y, particles[i].pos.x, particles[i].pos.y);
-            if (distance < maxDistance && distance > 0) {
+		toCheck = toCheck.concat(grid[gX - 1, gY - 1]);
+		toCheck = toCheck.concat(grid[gX,     gY - 1]);
+		toCheck = toCheck.concat(grid[gX + 1, gY - 1]);
+
+		toCheck = toCheck.concat(grid[gX - 1, gY]);
+		toCheck = toCheck.concat(grid[gX,     gY]);
+		toCheck = toCheck.concat(grid[gX + 1, gY]);
+
+		toCheck = toCheck.concat(grid[gX - 1, gY + 1]);
+		toCheck = toCheck.concat(grid[gX,     gY + 1]);
+		toCheck = toCheck.concat(grid[gX + 1, gY + 1]);
+
+        toCheck.flatten();
+
+	    console.log(toCheck);
+
+        for (i = 0; i < toCheck.length; i++) {
+            var distance = dist(this.pos.x, this.pos.y, particles[toCheck[i]].pos.x, particles[toCheck[i]].pos.y);
+            if (distance < maxDistance && distance > 0.01) {
                 neighbours.push(i);
             }
         }
